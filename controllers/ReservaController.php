d593c0da0d9ea737e0b41a44179da457f9778741
josh161001 <estrella161610@gmail.com>
2023-05-07 23:06:04 -0600

    archivo modificado

--- controllers/ReservaController.php
+++ controllers/ReservaController.php
@@ -7,9 +7,10 @@ use MVC\Router;
 class ReservaController{
     
     public static function index(Router $router) { 
+      
         session_start();
         autoenticado();
-                
+
         $router->render('reservaciones/index', [
           'nombre'=> $_SESSION['nombre'] ,
           'id'=> $_SESSION['id'],
